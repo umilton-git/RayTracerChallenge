42e85b474b1b3a202e17e2dec160b6581efda430
umilton-git <ulysses.github@protonmail.com>
2025-08-05 15:01:08 -0400

    Started on Ray creation

---
+++ src/main/java/com/ulysses/raytracer/Ray.java
@@ -0,0 +1,8 @@
+package com.ulysses.raytracer;
+
+public class Ray {
+    Tuple origin, direction;
+
+    public Ray(Tuple newOrigin, Tuple newDirection) { this.origin = newOrigin; this.direction = newDirection; }
+
+}
